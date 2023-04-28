42a64c3b138c5c3b8697d364beb7dbec564b0c8c
Sherryz707 <shahrbanobokhari@gmail.com>
2023-04-28 13:45:35 +0500

    requiered APIModule class

--- controller/tourHandler.js
+++ controller/tourHandler.js
@@ -1,5 +1,5 @@
 const Tour = require('./../models/tourModel');
-
+const APIFeatures = require('./../utils/apiFeatures');
 // exports.aliasTopTours = (req, res, next) => {
 //   req.query.limit = '5';
 //   req.query.sort = '-ratingsAverage,price';
@@ -16,57 +16,7 @@ exports.getAllTours = async (req, res) => {
   try {
     // creating API features Obj
     // eslint-disable-next-line no-unused-vars
-    class APIFeatures {
-      constructor(query, queryString) {
-        this.query = query;
-        this.queryString = queryString;
-      }
 
-      filter() {
-        const queryObj = { ...this.queryString };
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
-
-        //filtering start
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(
-          /\b(gte|gt|lt|lte)\b/g,
-          match => `$${match}`
-        ); //=> $gt
-        this.query = this.query.find(JSON.parse(queryStr));
-        return this;
-      }
-
-      sort() {
-        if (this.queryString.sort) {
-          const sortBy = this.queryString.sort.split(',').join();
-          this.query = this.query.sort(sortBy);
-        } else {
-          this.query = this.query.sort('-createdAt');
-        }
-        return this;
-      }
-
-      limitFields() {
-        if (this.queryString.fields) {
-          const fields = this.queryString.fields.split(',').join(' ');
-          console.log(fields);
-          this.query = this.query.select(fields);
-        } else {
-          this.query = this.query.select('-__v');
-        }
-        return this;
-      }
-
-      paginate() {
-        const page = this.queryString.page * 1 || 1;
-        const limit = this.queryString.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        this.query = this.query.skip(skip).limit(limit);
-        return this;
-      }
-    }
     // console.log(req.query);
     // const tour = await Tour.find(); //retunrs  a js array of all items;
     //build the query
